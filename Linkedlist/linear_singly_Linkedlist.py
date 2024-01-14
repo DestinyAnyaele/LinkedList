ef7d510e1d10fdafa05bff5be30063c10012383d
root <root@localhost>
2024-01-14 14:15:50 +0100

    changed all comparison operators to is and not since we are working with objects

--- Linkedlist/linear_singly_Linkedlist.py
+++ Linkedlist/linear_singly_Linkedlist.py
@@ -7,44 +7,43 @@ class LSL :
     self.head = None
   def Append(self,data) :
     current_node = self.head
-    if current_node == None :
+    if current_node is None :
       self.head = Node(data)
     else :
-      while current_node.next != None :
+      while current_node.next is not None :
         current_node = current_node.next
       current_node.next = Node(data)
   def Display(self) :
     linked_list = []
     current_node = self.head
-    if current_node == None :
+    if current_node is None :
       return linked_list
     else :
-      while current_node.next != None :
+      while current_node.next is not None :
         linked_list.append(current_node.data)
         current_node = current_node.next
       linked_list.append(current_node.data)
       return linked_list
   def Remove(self,element) :
     current_node = self.head
-    if current_node.data == element :
+    if current_node.data is element :
       self.head = self.head.next
       return None
-    while current_node.next != None :
-      if current_node.data == element :
-
+    while current_node.next is not None :
+      if current_node.data is element :
         previous_node.next = current_node.next
         return None
       previous_node,current_node = current_node,current_node.next
-    if current_node.data == element :
+    if current_node.data is element :
       previous_node.next = current_node.next
     else :
       raise ValueError
   def Length(self) :
     count = 0
     current_node = self.head
-    if current_node == None :
+    if current_node is None :
       return count
-    while current_node.next != None :
+    while current_node.next is not None :
       count += 1
       current_node = current_node.next
     count += 1
